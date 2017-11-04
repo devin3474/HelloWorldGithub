0867b379298b489e5985d238bda3cc13f2203432
devin3474 <devin3474@gmail.com>
2017-11-04 18:31:02 -0400

    Added user input

--- src/Print.java
+++ src/Print.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
 
 public class Print {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		System.out.println("What's your name?");
+		Scanner Reader = new Scanner(System.in);	
+		System.out.println("");		
+		String ans = Reader.nextLine();
+		
+		System.out.println("Hi " + ans + "!");
+		
+		
+		
 	}
 
 }
